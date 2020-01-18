c85e577a53c3ef1d53ebe5ebdda8c7a2be1761dd
redseb <dwarfslikepie@gmail.com>
2020-01-18 22:58:33 +0100

    Added search bar to browser

--- src/components/WindowBrowser.jsx
+++ src/components/WindowBrowser.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import downArrow from "./../images/downArrow.png";
 
 const defaultUrl = "https://www.pja.edu.pl/";
 
-export default function () {
+const WindowBrowser = () => {
+  const [url, setUrl] = useState(defaultUrl);
+  const [searchBarText, setSearchBarText] = useState(url);
+  return (
+    <div className="browserContainer">
+      <div>
+        <form
+          className="searchBar"
+          onSubmit={() => {
+            setUrl(searchBarText);
+            event.preventDefault();
+          }}
+        >
+          <label>
+            Address:
+            <input
+              type="text"
+              value={searchBarText}
+              onChange={event => {
+                setSearchBarText(event.target.value);
+              }}
+            />
+          </label>
+          <button type="submit">
+            <img className="system-bar-icon" src={downArrow} />
+          </button>
+        </form>
+      </div>
+      <iframe src={url} key={url} height={500} width={800}>
+        <base target="_parent" />
+      </iframe>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <iframe src={defaultUrl} height={500} width={800} />
-        </div>
-    );
-}
+export default WindowBrowser;
